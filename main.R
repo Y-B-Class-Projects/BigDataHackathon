4bb9c014d49e5b663f92d6af3dcbd9ada7632415
mickaelbalensi <57725660+mickaelbalensi@users.noreply.github.com>
2021-06-01 18:42:24 +0200

    question 3

--- main.R
+++ main.R
@@ -14,5 +14,27 @@ users.db <- dbConnect(RSQLite::SQLite(), "BX-Users_hkv1.db")
 users <- dbReadTable(users.db, "bx-users")
 head(users)
 
+#3a
+nb_users = nrow(users[!duplicated(users$User.ID),])
+#3b
+nb_books = nrow(books[!duplicated(books$ISBN),])
+#3c
+nb_ratings = nrow(ratings[ratings$Book.Rating,])
 
+#3d
+N = 5
+user_ratings=data.frame(table(ratings$User.ID))
+filter(user_ratings, Freq>=N )
+
+#3e
+N2 = 7
+book_ratings = data.frame(table(ratings$ISBN))
+filter(book_ratings, Freq>=N2 )
+
+#3f
+arrange(book_ratings, desc(Freq))
+
+#3g
+arrange(user_ratings, desc(Freq))
+#order_df=df[order(df$Freq, decreasing = TRUE),]
 
