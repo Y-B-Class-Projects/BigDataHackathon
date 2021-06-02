d272b6e1f0ae10e32d7934bb24b5372702011d2a
bakshtb <bakshtb@gmail.com>
2021-06-02 13:07:47 +0300

    change = to <-

--- main.R
+++ main.R
@@ -1,5 +1,5 @@
 #install.packages("RSQLite")
-install.packages("tidyverse")
+#install.packages("tidyverse")
 
 library("RSQLite")
 library("tidyverse")
@@ -7,31 +7,32 @@ library("tidyverse")
 
 books.db <- dbConnect(RSQLite::SQLite(), "BX-Books_hkv1.db")
 books <- dbReadTable(books.db, "bx-books")
-head(books)
+str(books)
 
 ratings.db <- dbConnect(RSQLite::SQLite(), "BX-Ratings_hkv1.db")
 ratings <- dbReadTable(ratings.db, "bx-book-ratings")
-head(ratings)
+str(ratings)
 
 users.db <- dbConnect(RSQLite::SQLite(), "BX-Users_hkv1.db")
 users <- dbReadTable(users.db, "bx-users")
-head(users)
+str(users)
+
 
 #3a
-nb_users = nrow(users[!duplicated(users$User.ID),])
+nb_users <- nrow(users[!duplicated(users$User.ID),])
 #3b
-nb_books = nrow(books[!duplicated(books$ISBN),])
+nb_books <- nrow(books[!duplicated(books$ISBN),])
 #3c
-nb_ratings = nrow(ratings[ratings$Book.Rating,])
+nb_ratings <- nrow(ratings[ratings$Book.Rating,])
 
 #3d
-N = 5
-user_ratings=data.frame(table(ratings$User.ID))
+N <- 5
+user_ratings <- data.frame(table(ratings$User.ID))
 filter(user_ratings, Freq>=N )
 
 #3e
-N2 = 7
-book_ratings = data.frame(table(ratings$ISBN))
+N2 <- 7
+book_ratings <- data.frame(table(ratings$ISBN))
 filter(book_ratings, Freq>=N2 )
 
 #3f
