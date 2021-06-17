bcd51b96b0eaa87c55e420da39f286d54e6cc690
bakshtb <bakshtb@gmail.com>
2021-06-17 18:08:30 +0300

    debugging

--- main.R
+++ main.R
@@ -46,8 +46,8 @@ my.predict <- function (model, newdata, n = 10, data = NULL,
     if (!is.matrix(s_uk)) s_uk <- as.matrix(t(s_uk))
     ratings <- t(sapply(1:nrow(newdata), FUN = function(i) {
       r_neighbors <- as(model$data[neighbors[[i]]], "dgCMatrix")
-      Cols.1 <- drop(as(crossprod(r_neighbors, s_uk[, i]), "matrix"))
-      Cols.2 <- drop(as(crossprod(!dropNAis.na(r_neighbors), s_uk[, i]), "matrix"))
+      Cols.1 <- drop(as(crossprod(r_neighbors, as.numeric(as.data.frame(s_uk[, i])[,])), "matrix"))
+      Cols.2 <- drop(as(crossprod(!dropNAis.na(r_neighbors), as.numeric(as.data.frame(s_uk[, i])[,])), "matrix"))
       Cols.1 / Cols.2
     }))
     ratings[!is.finite(ratings)] <- NA
@@ -101,42 +101,38 @@ ratings <- ratings[(ratings$ISBN %in% books$ISBN),] #remove unknowns ISBNs from 
 ratings <- ratings[(ratings$User.ID %in% users$User.ID),] #remove unknowns ISBNs from ratings.
 
 # To only save users that gived grades to more than N books and return in rating_test2
-min.rating.user <- 10
-min.rating.book <- 10
+min.rating.user <- 2
+min.rating.book <- 2
 
 
-book.freq.ratings <- data.frame(table(ratings$ISBN))
-freq.book <- filter(book.freq.ratings, Freq >= min.rating.book )
-colnames(freq.book)<- c("ISBN", "Frequence")
-ratings <- filter(ratings, ISBN %in% freq.book$ISBN)
+#book.freq.ratings <- data.frame(table(ratings$ISBN))
+#freq.book <- filter(book.freq.ratings, Freq >= min.rating.book )
+#colnames(freq.book)<- c("ISBN", "Frequence")
+#ratings <- filter(ratings, ISBN %in% freq.book$ISBN)
 
-user.freq.ratings <- data.frame(table(ratings$User.ID)) # give frequence of ID_User
-freq.user <- filter(user.freq.ratings, Freq >= min.rating.user ) # take only user that appears more of N
-freq.user <- arrange(freq.user, desc(Freq)) # arrange with big freq at the beginning
-colnames(freq.user)<- c("ID", "Frequence")
-ratings<- filter(ratings, User.ID %in% freq.user$ID)
-
-
-ratings.matrix <- matrix(
-  nrow = length(freq.user$ID),
-  ncol = length(freq.book$ISBN),
-  dimnames = list(freq.user$ID, freq.book$ISBN)
-)
+#user.freq.ratings <- data.frame(table(ratings$User.ID)) # give frequence of ID_User
+#freq.user <- filter(user.freq.ratings, Freq >= min.rating.user ) # take only user that appears more of N
+#freq.user <- arrange(freq.user, desc(Freq)) # arrange with big freq at the beginning
+#colnames(freq.user)<- c("ID", "Frequence")
+#ratings<- filter(ratings, User.ID %in% freq.user$ID)
 
 
 
-for (i in 1:dim(ratings)[1]){
-  ratings.matrix[toString(ratings[i,1]), ratings[i,2]] <- ratings[i, 3]
+real.rating.matrix <- as(ratings, "realRatingMatrix")
+
+
+while(min(rowCounts(real.rating.matrix)) < min.rating.user || min(colCounts(real.rating.matrix)) < min.rating.book){
+  real.rating.matrix <- real.rating.matrix[rowCounts(real.rating.matrix) >= min.rating.user, colCounts(real.rating.matrix) >= min.rating.book]
+  cat(".")
 }
 
-
-real.rating.matrix <- as(ratings.matrix, "realRatingMatrix")
-rm(ratings.matrix)
-
+cat(paste("\nMin rating user", min(rowCounts(real.rating.matrix)), "\n" ,collapse = " "))
+cat(paste("Min rating book", min(colCounts(real.rating.matrix)) , "\n" ,collapse = " "))
+cat(paste("Dim of rating matrix is:", real.rating.matrix@data@Dim[1], ":", real.rating.matrix@data@Dim[2], "(", object.size(real.rating.matrix)/1000000000 , "Gb)\n" ,collapse = " "))
 
 eval_sets <- evaluationScheme(data = real.rating.matrix, method = "split",
-                              train = 0.8, given = 10,
-                              goodRating = 3, k = 5)
+                              train = 0.8, given = min.rating.user,
+                              goodRating = 5, k = 5)
 
 
 eval_recommender <- Recommender(data = getData(eval_sets, "train"),
@@ -152,39 +148,42 @@ M <- round( eval_prediction@data, digits = 1 )
 
 colnames(M) <- sapply( colnames(M), function(c) books[books$ISBN == c,]$Book.Title )
 
-print(M)
+#print(M)
 
 final <- as.matrix(M)
 
+accuracy.final <- calcPredictionAccuracy(x = eval_prediction,
+                                        data = getData(eval_sets, "unknown"))
+head(accuracy.final)
 
 
 #3a
-nb_users <- nrow(users[!duplicated(users$User.ID),])
+#nb_users <- nrow(users[!duplicated(users$User.ID),])
 #3b
-nb_books <- nrow(books[!duplicated(books$ISBN),])
+#nb_books <- nrow(books[!duplicated(books$ISBN),])
 #3c
-nb_ratings <- nrow(ratings[ratings$Book.Rating,])
+#nb_ratings <- nrow(ratings[ratings$Book.Rating,])
 
 #3d
-N <- 50
-user_ratings <- data.frame(table(ratings$User.ID))
-freq.user <- filter(user_ratings, Freq >= N )
-freq.user <- arrange(freq.user, desc(Freq))
-colnames(freq.user)<- c("ID", "Frequence")
+#N <- 50
+#user_ratings <- data.frame(table(ratings$User.ID))
+#freq.user <- filter(user_ratings, Freq >= N )
+#freq.user <- arrange(freq.user, desc(Freq))
+#colnames(freq.user)<- c("ID", "Frequence")
 
-filter(ratings, User.ID %in% freq.user$ID)
+#filter(ratings, User.ID %in% freq.user$ID)
 
 
 #3e
-N2 <- 7
-book.freq.ratings <- data.frame(table(ratings$ISBN))
-filter(book.freq.ratings, Freq >= N2 )
+#N2 <- 7
+#book.freq.ratings <- data.frame(table(ratings$ISBN))
+#filter(book.freq.ratings, Freq >= N2 )
 
 #3f
-arrange(book.freq.ratings, desc(Freq))
+#arrange(book.freq.ratings, desc(Freq))
 
 #3g
-arrange(user_ratings, desc(Freq))
+#arrange(user_ratings, desc(Freq))
 #order_df<-df[order(df$Freq, decreasing <- TRUE),]
 
 
